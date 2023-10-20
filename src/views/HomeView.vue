7f5e8c1534c902a59089977b4589f89d3d249a80
ADNANE-RBT <92606656+ADNANE-RBT@users.noreply.github.com>
2023-10-20 01:18:15 +0100

    second section done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -60,6 +60,46 @@
       </div>
     </section>
 
+    <!-- second section -->
+    <section class=" mt-30 p-24 2xl:w-9/12 mx-auto">
+      <div class="bg-main rounded-lg flex flexwrap md:flex-nowrap md:h-96">
+        <div class=" w-full md:w-1/3 h-full ">
+          <div class=" md:h-3/4 flex flex-nowrap justify-center items-center -space-x-11 ">
+            <div class="h-full flex items-end pb-10 z-10 ">
+              <svg xmlns="http://www.w3.org/2000/svg" width="54" height="48" viewBox="0 0 54 48" fill="none">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M19.0743 6.60921C20.5153 5.46357 20.755 3.36664 19.6092 1.92561C18.4636 0.484573 16.3667 0.245073 14.9256 1.39074C2.78235 11.0447 0.333313 23.5946 0.333313 37.3333C0.333313 42.8563 4.81048 47.3333 10.3333 47.3333C15.8561 47.3333 20.3333 42.8563 20.3333 37.3333C20.3333 31.8106 15.8561 27.3333 10.3333 27.3333C9.37441 27.3333 8.44705 27.4683 7.56918 27.7203C8.72065 19.2906 11.8579 12.3463 19.0743 6.60921ZM53.6666 37.3333C53.6666 42.8563 49.1896 47.3333 43.6666 47.3333C38.1436 47.3333 33.6666 42.8563 33.6666 37.3333C33.6666 23.5946 36.1157 11.0447 48.259 1.39074C49.7 0.245073 51.797 0.484573 52.9426 1.92561C54.0883 3.36664 53.8486 5.46357 52.4076 6.60921C45.1913 12.3463 42.054 19.2906 40.9026 27.7203C41.7803 27.4683 42.7076 27.3333 43.6666 27.3333C49.1896 27.3333 53.6666 31.8106 53.6666 37.3333Z" fill="#FBFBFB"/>
+              </svg>
+            </div>
+           <div>
+           <img src="@/assets/pdirector.jpeg" alt="" class="rounded-full w-56 h-56 ">
+
+           </div> 
+          </div>
+          <div class=" md:h-1/4 md:w-full text-white text-center">
+               <div class="font-bold text-3xl pb-1">Mostafa Ezziyyani </div>
+            <p class="text-sm">Professor </p> 
+ 
+                      
+          </div>
+        </div>
+        <div class=" w-full md:w-2/3 h-full md:px-6  ">
+          <!-- header -->
+          <div class=" w-full px-6">
+            <h1 class="w-auto rounded-b-2xl shadow-md bg-white text-main text-center p-2 font-bold text-2xl"> Word from the Program Director :</h1>
+          </div>
+          <!-- the paragraph -->
+          <div class="w-full px-4 py-5 text-white">
+            <p> <span class="text-black text-lg " >"</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis ea beatae illum quidem ipsa asperiores mollitia, odit cupiditate provident, excepturi autem vitae quisquam tenetur quae fuga praesentium doloremque eum ad.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quos recusandae quo, mollitia dolor blanditiis corrupti obcaecati architecto pariatur animi omnis nesciunt esse quis, officia doloribus harum, nemo magni accusamus?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quos recusandae quo, mollitia dolor blanditiis corrupti obcaecati architecto pariatur animi omnis nesciunt esse quis, officia doloribus harum, nemo magni accusamus?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quos recusandae quo, mollitia dolor blanditiis corrupti obcaecati architecto pariatur animi omnis nesciunt esse quis, officia doloribus harum, nemo magni accusamus?
+              <span class="text-black text-lg " >"</span> 
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
 
 </template>
 
