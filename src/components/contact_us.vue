9c0d1b5adcf393a7caa4a17a280b18c56c647ed0
ADNANE-RBT <92606656+ADNANE-RBT@users.noreply.github.com>
2023-10-24 00:40:15 +0100

    last forgotten change before deploying

---
+++ src/components/contact_us.vue
@@ -0,0 +1,48 @@
+<template lang="">
+  <div  style="font-family: 'Lato', sans-serif; "
+    class="p-6  overflow-x-hidden shadow-md bg-slate-50 ">
+    <div
+      class="p-6 container  md:w-1/3 xl:w-auto mx-auto flex flex-col xl:items-stretch justify-between xl:flex-row"
+    >
+      <div
+        class="xl:w-2/5  xl:mb-0 relative  h-auto flex items-center justify-center"
+      >
+        <img
+          src="https://cdn.tuk.dev/assets/components/26May-update/newsletter-1.png"
+          alt="Envelope with a newsletter"
+          role="img"
+          class=" xl:w-full lg:w-1/2 w-full"
+        />
+      </div>
+      <div class="w-full xl:w-3/5 xl:px-8 xl:py-28 ">
+        <h1
+          class="text-2xl md:text-4xl xl:text-5xl font-bold leading-10 text-gray-800 mb-4 text-center xl:text-left md:mt-0 mt-4"
+        >
+          Contact us
+        </h1>
+        <p
+          class="text-base leading-normal text-gray-600 text-center xl:text-left"
+        >
+        Get in touch with us if you have any inqueries
+        </p>
+        <div class="flex items-stretch mt-12 shadow-md">
+          <input
+            class="bg-gray-100  rounded-lg rounded-r-none text-base leading-none text-gray-800 p-5 w-full border border-transparent focus:outline-none focus:border-gray-500"
+            type="email"
+            placeholder="Your Email"
+          />
+          <button
+            class=" min-w-min  rounded-l-none hover:bg-main/100 bg-main/70 rounded text-base font-medium leading-none text-white p-5 uppercase focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700"
+          >
+            Contact us
+          </button>
+        </div>
+      </div>
+    </div>
+</div>
+</template>
+<script setup>
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/Ourstudents.vue
@@ -0,0 +1,197 @@
+<template>
+    <div class="bg-white py-24 sm:py-32">
+      <div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+        <div class="max-w-2xl">
+          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our Students</h2>
+          <p class="mt-6 text-lg leading-8 text-gray-600">Meet our exceptional students, the driving force behind our academic community. With their unwavering dedication and passion for learning, they are at the forefront of innovation and excellence. Our diverse student body represents a wealth of backgrounds and experiences, creating a dynamic and enriching environment for collaborative learning.</p>
+        </div>
+        <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols- md:grid-cols-3 2xl:grid-cols-4 sm:gap-y-16 xl:col-span-2">
+          <li v-for="student in people" :key="student.name">
+            <div class="flex items-center gap-x-6">
+              <img class="h-16 w-16 rounded-full" :src="student.imageUrl" alt="" />
+              <div>
+                <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">{{ student.name }}</h3>
+                <p class="text-sm font-semibold leading-6 text-indigo-600">{{ student.role }}</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+const people = [
+  {
+    name: 'Riyadi Adnane',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Riyadi_Adnane.png',
+  },
+  {
+    name: 'Daghmoumi Marwan',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Daghmoumi_Marwan.png',
+  },
+  {
+    name: 'Abdelali IBN TABET',
+    role: 'Student',
+    imageUrl: '/src/assets/students/IBN TABET_ABDELALI.png',
+  },
+  {
+    name: 'Ahmed Samady',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Samady_Ahmed.png',
+  },
+  {
+    name: 'Aicha Kharbach',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Kharbach_Aicha.png',
+  },
+  {
+    name: 'Aymen Boufarhi',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Boufarhi_Ayman.png',
+  },
+  {
+    name: 'Ismail Drief',
+    role: 'Student',
+    imageUrl: '/src/assets/students/DRIEF_ISMAIL.png',
+  },
+  {
+    name: 'Loubna El Ghazi',
+    role: 'Student',
+    imageUrl: '/src/assets/students/EL_GHAZI_Loubna.png',
+  },
+  {
+    name: 'Mohamed Amine Sabbahi',
+    role: 'Student',
+    imageUrl: '/src/assets/students/SABBAHI_Mohamed_Amine.png',
+  },
+  {
+    name: 'Yasmina Hachhouch',
+    role: 'Student',
+    imageUrl: '/src/assets/students/hachhouch_yasmina.png',
+  },
+  {
+    name: 'AACHABI mohammed',
+    role: 'Student',
+    imageUrl: '/src/assets/students/AACHABI_mohammed.png',
+  },
+  {
+    name: 'El Meziane Chaima',
+    role: 'Student',
+    imageUrl: '/src/assets/students/El_Meziane_Chaima.png',
+  },
+  {
+    name: 'fakher-eddine mohamed amine',
+    role: 'Student',
+    imageUrl: '/src/assets/students/fakher-eddine_mohamed_amine.png',
+  },
+  {
+    name: 'Imasbahen khaoula',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Imasbahen_khaoula.png',
+  },
+  {
+    name: 'Liefrid Chihab-Eddine',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Liefrid_Chihab-Eddine.png',
+  },
+  {
+    name: 'Omar Nouih',
+    role: 'Student',
+    imageUrl: '/src/assets/students/NOUIH_Omar.png',
+  },
+  {
+    name: 'Hanane Zaoui',
+    role: 'Student',
+    imageUrl: '/src/assets/students/ZAOUI_Hanane.png',
+  },
+  {
+    name: 'Yasmine Zerbet',
+    role: 'Student',
+    imageUrl: '/src/assets/students/ZERBET_Yasmine.png',
+  },
+  {
+    name: 'Abdelmajid Benjelloun',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Abdelmajid_benjelloun.png',
+  },
+  {
+    name: 'Aymane Mahri',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Mahri_Aymane.png',
+  },
+  {
+    name: 'Mohamed Mohidine',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Mohidine_mohamed.png',
+  },
+  {
+    name: 'Salma Nidar',
+    role: 'Student',
+    imageUrl: '/src/assets/students/nidar_salma.png',
+  },
+  {
+    name: 'Nouhaila Bezzar',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Nouhaila_BEZZAR.png',
+  },
+  {
+    name: 'ARICHI_Fatima zahra',
+    role: 'Student',
+    imageUrl: '/src/assets/students/ARICHI_Fatima zahra.png'
+  },
+  {
+    name: 'Aymen_AitAhmed',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Aymen_AitAhmed.png'
+  },
+  {
+    name: 'BAKKALI_AYOUB',
+    role: 'Student',
+    imageUrl: '/src/assets/students/BAKKALI_AYOUB.png'
+  },
+  {
+    name: 'chibani_fahd',
+    role: 'Student',
+    imageUrl: '/src/assets/students/chibani_fahd.png'
+  },
+  {
+    name: 'Elaoufi_Imane',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Elaoufi_Imane.png'
+  },
+  {
+    name: 'Essetti_Widad',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Essetti_Widad.png'
+  },
+  {
+    name: 'Hanae_Elmrabet',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Hanae_Elmrabet.png'
+  },
+  {
+    name: 'Idrissi_hamza',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Idrissi_hamza.png'
+  },
+  {
+    name: 'Idriss_khattabi',
+    role: 'Student',
+    imageUrl: '/src/assets/students/Idriss_khattabi.png'
+  },
+  {
+    name: 'KRIMEL_NOUHAILA',
+    role: 'Student',
+    imageUrl: '/src/assets/students/KRIMEL_NOUHAILA.png'
+  },
+  {
+    name: 'ZAOUI_Hanane',
+    role: 'Student',
+    imageUrl: '/src/assets/students/ZAOUI_Hanane.png'
+  }
+];
+
+  </script>
